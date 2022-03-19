2351e05402d79191773aea6ed55bcdc34a3620e5
fsc1118 <70362920+fsc1118@users.noreply.github.com>
2022-03-18 23:17:18 -0400

    Update leetcode9.rb

--- leetcode9.rb
+++ leetcode9.rb
@@ -1,11 +1,14 @@
 # @param {Integer} x
 # @return {Boolean}
 def is_palindrome(x)
+    if (x < 0)
+        return false 
+    end
     temp = x
     revertedNumber = 0
-    while temp != 0:
+    while temp != 0
         num = temp % 10
-        revertedNumber = revertedNumber * 10 + revertedNumber
+        revertedNumber = revertedNumber * 10 + num
         temp /= 10
     end
     return revertedNumber == x
